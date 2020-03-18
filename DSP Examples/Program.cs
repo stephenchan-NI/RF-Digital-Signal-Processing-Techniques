6b01b5648a4eafb2a26a182566da127a5cf036eb
Stephen Chan <stephen.chan@ni.com>
2020-03-18 15:09:15 -0500

    Adding initial files

---
+++ DSP Examples/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DSP_Examples
+{
+    class Program
+    {
+        static void Main()
+        {
+            int sampleRate = 100; //Baseband Samples/Second
+            int numberSamples = 1000; //Samples
+            double bbFrequency = 10; //Baseband cycles/Second
+            double rfFrequency = 1e9;
+            double[] iValues = new double[numberSamples];
+            double[] qValues = new double[numberSamples];
+            initializeCW(numberSamples, sampleRate, bbFrequency, iValues, qValues);
+            int k = 0;
+            foreach(double j in iValues)
+            {
+                Console.WriteLine(j);
+            }
+        }
+
+        //ToDo: Filters, Windowing 
+        static void initializeCW(int numberSamples, int sampleRate, double bbFrequency, double[] iValues, double[] qValues)
+        {
+            for (int i = 0; i < numberSamples; i++)
+            {
+               iValues[i] = Math.Cos(2 * Math.PI * bbFrequency * i / sampleRate);
+               qValues[i] = Math.Sin(2 * Math.PI * bbFrequency * i / sampleRate);
+            }
+        }
+    }
+}
